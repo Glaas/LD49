62c892bde4e1397f5ff41d29f96bbb2bbf68bf6d
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2021-10-02 00:13:09 +0200

    first commit

---
+++ Assets/TestClass.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestClass : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nameof(rotate));
+
+    }
+
+    IEnumerator rotate()
+    {
+
+        transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+        yield return new WaitForSeconds(0.5f);
+        var col = new Color(Random.Range(0f, .1f), Random.Range(0f, .1f), Random.Range(0f, .1f));
+        GetComponent<MeshRenderer>().material.color = col;
+        StartCoroutine(nameof(rotate));
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
